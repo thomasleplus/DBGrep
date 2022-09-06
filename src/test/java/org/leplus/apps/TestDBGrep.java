060a521c6f653d13d9f309a6d5be20f763f9038d
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 19:09:46 -0700

    Test

---
+++ src/test/java/org/leplus/apps/TestDBGrep.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.leplus.apps;
+
+import org.junit.Test;
+
+/**
+ * @author thomas
+ *
+ */
+public class TestDBGrep {
+
+    @Test
+    public void test() {
+        DBGrep.main(new String[] { "-h" });
+    }
+
+}
