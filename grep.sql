2b7e53a6d52ac618f4f5e02ed5efa37565c4c6dd
Thomas Leplus <thomasleplus@users.noreply.github.com>
2023-08-22 20:36:34 -0700

    Complete rewrite in Oracle PL/SQL

--- grep.sql
+++ grep.sql
@@ -1,31 +1,13 @@
 CREATE OR REPLACE PROCEDURE grep (
-  username IN VARCHAR2,
-  pattern IN VARCHAR2
+    username IN VARCHAR2,
+    pattern IN VARCHAR2
 ) IS
-  matches DBMS_SQL.VARCHAR2_TABLE;
+    m DBMS_SQL.VARCHAR2_TABLE;
 BEGIN
-    FOR t IN (SELECT table_name,
-                     column_name
-              FROM   all_tab_cols
-              WHERE  owner = username
-                     AND data_type = 'VARCHAR2') LOOP
-        EXECUTE IMMEDIATE
-'SELECT "'||t.column_name||'" FROM "'||t.table_name||'" WHERE "'||t.column_name||'" LIKE :1'
-    BULK COLLECT INTO matches
-    USING pattern;
-
-FOR i IN 1 .. matches.COUNT LOOP
-  dbms_output.put_line('"'
-                       || pattern
-                       ||'","'
-                       || username
-                       ||'","'
-                       || t.table_name
-                       ||'","'
-                       ||t.column_name
-                       ||'","'
-                       ||matches(i)
-                       ||'"');
-END LOOP;
-END LOOP;
+    FOR t IN (SELECT table_name, column_name FROM all_tab_cols WHERE owner = username AND data_type = 'VARCHAR2') LOOP
+        EXECUTE IMMEDIATE 'SELECT "' || t.column_name || '" FROM "' || t.table_name || '" WHERE "' || t.column_name || '" LIKE :1' BULK COLLECT INTO m USING pattern;
+        FOR i IN 1 .. matches.COUNT LOOP
+            DBMS_OUTPUT.PUT_LINE('"' || pattern || '","' || username || '","' || t.table_name || '","' || t.column_name || '","' || matches(i) || '"');
+        END LOOP;
+    END LOOP;
 END grep;
