2b623367fb4578507bf44ec8ea9d765f028789dd
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-09 23:03:01 +0200

    style(sql): linting

--- grep.sql
+++ grep.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE PROCEDURE grep (
+CREATE OR REPLACE PROCEDURE grep(
     username IN VARCHAR2,
     pattern IN VARCHAR2
-) IS
-    m DBMS_SQL.VARCHAR2_TABLE;
+) AS
+m DBMS_SQL.VARCHAR2_TABLE;
 BEGIN
     FOR t IN (SELECT table_name, column_name FROM all_tab_cols WHERE owner = username AND data_type = 'VARCHAR2') LOOP
         EXECUTE IMMEDIATE 'SELECT "' || t.column_name || '" FROM "' || t.table_name || '" WHERE "' || t.column_name || '" LIKE :1' BULK COLLECT INTO m USING pattern;
--- test.sql
+++ test.sql
@@ -1,15 +1,39 @@
-@grep.sql
+@ grep.sql
 
 CREATE TABLE employees (
-  employee_id INT,
-  full_name VARCHAR2(64),
-  first_name VARCHAR2(32),
-  last_name VARCHAR2(32),
-  email_address VARCHAR2(64)
+    employee_id INT,
+    full_name VARCHAR2(64),
+    first_name VARCHAR2(32),
+    last_name VARCHAR2(32),
+    email_address VARCHAR2(64)
 );
 
-INSERT INTO employees (employee_id, full_name, first_name, last_name, email_address) VALUES (1, 'John Doe', 'John', 'Doe', 'john.doe@example.com');
+INSERT INTO employees (
+    employee_id,
+    full_name,
+    first_name,
+    last_name,
+    email_address
+) VALUES (
+    1,
+    'John Doe',
+    'John',
+    'Doe',
+    'john.doe@example.com'
+);
 
-INSERT INTO employees (employee_id, full_name, first_name, last_name, email_address) VALUES (2, 'Jane Doe', 'Jane', 'Doe', 'jane.doe@example.com');
+INSERT INTO employees (
+    employee_id,
+    full_name,
+    first_name,
+    last_name,
+    email_address
+) VALUES (
+    2,
+    'Jane Doe',
+    'Jane',
+    'Doe',
+    'jane.doe@example.com'
+);
 
 EXECUTE grep('HR', '%John%');
